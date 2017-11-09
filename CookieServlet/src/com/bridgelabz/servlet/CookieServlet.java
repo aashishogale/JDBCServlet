772b2b628ad55e0ee9ccb6dd04d61177f13a7165
aashishogale <aashishogale@gmail.com>
2017-11-09 19:30:48 +0530

    9 november

---
+++ CookieServlet/src/com/bridgelabz/servlet/CookieServlet.java
@@ -0,0 +1,54 @@
+package com.bridgelabz.servlet;
+
+import java.io.IOException;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CookieServlet
+ */
+
+public class CookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CookieServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie cookie=new Cookie("firstname", request.getParameter("Name"));
+		
+		
+		response.addCookie(cookie);
+		
+
+		RequestDispatcher requestDispatcher=getServletContext().getRequestDispatcher("/ReadCookie"); 
+		requestDispatcher.forward(request, response);
+		
+		
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
